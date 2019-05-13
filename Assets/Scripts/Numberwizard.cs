968879c79bf60582936bf3fad506678055f008bc
Warin12 <evanw8@learn.cssd.ab.ca>
2019-05-13 14:44:49 -0600

    FIANL COMMIT

---
+++ Assets/Scripts/Numberwizard.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Numberwizard : MonoBehaviour
+{
+    [SerializeField]int max;
+    [SerializeField]int min;
+    [SerializeField]TextMeshProUGUI guessText;
+    int guess;
+   
+    void Start()
+    {
+        StartGame(); 
+    }
+    void StartGame()
+    {
+        NextGuess();
+    }
+    public void OnPressHigher()
+    {
+        min = guess + 1; 
+        NextGuess();
+    }
+    public void OnPressLower()
+    {
+        max = guess - 1; 
+        NextGuess();
+    }
+    void NextGuess()
+    {   
+    guess = Random.RandomRange(min, max + 1);
+    guessText.text = guess.ToString();
+    }
+
+}
